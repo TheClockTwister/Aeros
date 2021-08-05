dc9caf603a3e801de4757b073d442fb97481c12e
TheClockTwister <wiesen.leon@gmail.com>
2021-08-05 11:15:37 +0200

    remove misc submodule

--- aeros/__init__.py
+++ aeros/__init__.py
@@ -1,5 +1,4 @@
 from .WebServer import WebServer
-from .misc import *
 from .thread import AdvancedThread
 from .compression import Gzip, Br
 from .caching import *
--- aeros/misc.py
+++
@@ -1,6 +0,0 @@
-"""
-The misc module contains useful helper functions and decorators to simplify development and make
-the resulting code much more readable and clean.
-"""
-
-from asgiref.sync import sync_to_async, async_to_sync
