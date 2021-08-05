50a8427e92dec53d7553fa0132991e72111ee44d
TheClockTwister <wiesen.leon@gmail.com>
2021-08-05 09:38:44 +0200

    Remove EasyRequest and ensure_async

--- aeros/WebServer.py
+++ aeros/WebServer.py
@@ -18,37 +18,7 @@ from .caching.server import Cache
 from .compression import Base
 from .Request import EasyRequest
 
-from asgiref.sync import sync_to_async, SyncToAsync
-from typing import Callable, Coroutine, Any
 from functools import wraps
-
-
-def ensure_async(f: Callable, callback_on_sync: Callable = None):
-    # -> Callable[[Any], Coroutine[Any, Any, Any]]
-    """
-    Ensures, that a supplied function is asynchronous. Detects Python-native async
-    functions and functions wrapped with `asgiref.sync.sync_to_async()`.
-
-    Arguments:
-        f (Callable): The function that should be ensured async
-        callback_on_sync (Callable): Called if a function is converted to async
-
-    Returns:
-        Callable[..., Coroutine[...]]: An async version of the supplied function
-    """
-
-    #
-
-    if inspect.iscoroutinefunction(f) or type(f) == SyncToAsync:
-        return f
-
-    @wraps(f)
-    async def wrapper(*args, **kwargs):
-        return await sync_to_async(f)(*args, **kwargs)
-
-    if callback_on_sync:
-        callback_on_sync()
-    return wrapper
 
 
 class WebServer(Quart):
@@ -267,12 +237,8 @@ class WebServer(Quart):
 
         def new_route_decorator(func):
             try:
-                new_func = ensure_async(
-                    func,
-                    callback_on_sync=lambda: self.logger.warning(f'Endpoint function "{func.__name__}" for "{rule}" is not asynchronous.')
-                )
-                new_func = EasyRequest()(new_func)  # inject EasyRequest into every function so that it is always available
-                Quart.route(self, *args, **kwargs)(new_func)  # route the new function to Quart.route
+                Quart.route(self, *args, **kwargs)(func)  # route the new function to Quart.route
+                return func
             except Exception as e:
                 self.logger.critical(f'Endpoint function "{func.__name__}" for "{rule}" could not be routed: ' + str(e))
                 raise e
--- aeros/__init__.py
+++ aeros/__init__.py
@@ -1,6 +1,5 @@
 from .WebServer import WebServer
 from .misc import *
-from .Request import EasyRequest
 from .thread import AdvancedThread
 from .compression import Gzip, Br
 from .caching import *
